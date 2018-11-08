8b08f2b9cd6c7f6f3e055014bb5d48c62ce8c014
i13302 <s16t286@stu.kagawa-u.ac.jp>
2018-11-08 19:58:56 +0900

    大量のボタンを表示

--- roulette/src/roulette/Table.java
+++ roulette/src/roulette/Table.java
@@ -1,7 +1,51 @@
 package roulette;
 
-public class Table {
-	Table() {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Table extends JPanel implements ActionListener {
+	private int numNumber=36+2;
+	
+	private Point mouse=new Point();
+	
+	private JButton[] JBtnNumber=new JButton[numNumber];
+	private JButton JBtnExit=new JButton("Exit");
+	
+	public Table() {
 		System.out.println("Table Class");
+		_init_();
+	}
+	
+	private void _init_() {
+		System.out.println("_init_");
+		for(int i=0;i<numNumber;i++) {
+			JBtnNumber[i]=new JButton(String.valueOf(i+1));
+			JBtnNumber[i].addActionListener(this);
+			add(JBtnNumber[i]);
+		}
+		JBtnExit.addActionListener(this);
+		add(JBtnExit);
+		
+		setLayout(new FlowLayout());
+	}
+	
+	public void mouseClicked(MouseEvent e) { 
+		mouse.x = e.getX();
+		mouse.y = e.getY();
+		repaint();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+//		 TODO Auto-generated method stub
+		System.out.println("actionPerformed");
+		String pushBtn = e.getActionCommand();
+		if(pushBtn=="Exit") {
+			System.out.println("Bye");
+			System.exit(0);
+		}
+		System.out.println(pushBtn);
+		
 	}
 }
--- roulette/src/roulette/roulette.java
+++ roulette/src/roulette/roulette.java
@@ -1,9 +1,26 @@
 package roulette;
 
-public class roulette {
+import roulette.Table;
+import javax.swing.JFrame;
+//import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+public class roulette{
 	public static void main(String args[]) {
 		System.out.println("Hello! World!");
-		Table table=new Table();
+		SwingUtilities.invokeLater(() -> {
+			JFrame frame = new JFrame("Table");
+			frame.add(new Table());
+			frame.pack();
+			frame.setVisible(true);
+			frame.setSize(600,400);
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		});
+		
+		
+//		Table table=new Table();
+		
+		
 		
 	}
 	
