bd26c92ce835f1c089d51a6563c483d0704c99c3
i13302 <s16t286@stu.kagawa-u.ac.jp>
2018-11-08 20:28:32 +0900

    お金用のボタン

--- roulette/src/roulette/Table.java
+++ roulette/src/roulette/Table.java
@@ -6,51 +6,74 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Table extends JPanel implements ActionListener {
-	private int numNumber=36+2;
+	private int numNumber=36+2; // 賭ける場所の個数
+	private int numMoney=4;     // 賭けるお金のパターン
 	
-	private Point mouse=new Point();
+	private JButton[] JBtnNumber=new JButton[numNumber]; // 場所のボタン
+	private JButton[] JBtnMoney =new JButton[numMoney];  // お金のボタン
+	private JButton JBtnExit=new JButton("Exit");        //　強制終了 
 	
-	private JButton[] JBtnNumber=new JButton[numNumber];
-	private JButton JBtnExit=new JButton("Exit");
+	private int statusNumber=-1;  // 選択した場所
+	private int statusMoney=-1;   // 選択したお金
 	
+	/* Constructer */
 	public Table() {
 		System.out.println("Table Class");
 		_init_();
 	}
 	
+	/* 初期処理 */
 	private void _init_() {
 		System.out.println("_init_");
+//		setLayout(null); 
+		setLayout(new FlowLayout()); // レイアウトの設定
+		
 		_setJBtnNumbers();
 		_setJBtnExit();
+		_setJBtnMoneys();
 		
-		setLayout(new FlowLayout());
+
 	}
 	
+	/* 賭ける場所のボタン */
 	private void _setJBtnNumbers() {
 		System.out.println(Util.getMethodName());
 		for(int i=0;i<numNumber;i++) {
-			JBtnNumber[i]=new JButton(String.valueOf(i+1));
+			JBtnNumber[i]=new JButton("Num "+String.valueOf(i+1));
 			JBtnNumber[i].addActionListener(this);
 			add(JBtnNumber[i]);
 		}
 	}
 	
+	/* 賭けるお金のボタン */
+	private void _setJBtnMoneys() {
+		System.out.println(Util.getMethodName());
+		JBtnMoney[0]=new JButton("Money "+String.valueOf(1));
+		JBtnMoney[1]=new JButton("Money "+String.valueOf(5));
+		JBtnMoney[2]=new JButton("Money "+String.valueOf(15));
+		JBtnMoney[3]=new JButton("Money "+String.valueOf(50));
+		for(int i=0;i<numMoney;i++) {
+		add(JBtnMoney[i]);
+		}
+	}
+	
+	/* 強制終了ボタン */
 	private void _setJBtnExit() {
 		System.out.println(Util.getMethodName());
 		JBtnExit.addActionListener(this);
 		add(JBtnExit);
 	}
 
+	/* イベント入力時 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 //		 TODO Auto-generated method stub
 		System.out.println("actionPerformed");
-		String pushBtn = e.getActionCommand();
-		if(pushBtn=="Exit") {
-			System.out.println("Bye");
-			System.exit(0);
-		}
+		String pushBtn = e.getActionCommand(); // 押したボタンの表示名(String)
 		System.out.println(pushBtn);
 		
+		if(pushBtn.equals("Exit")) {
+			System.exit(0);
+		}
 	}
 }
