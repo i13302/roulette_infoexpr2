98da17173f3c8115d51326cc09f434c9ba3592dd
guni973 <ad2314ce71926@gmail.com>
2018-11-13 00:57:18 +0900

    fix: eclipse format

--- roulette/src/Progress.java
+++ roulette/src/Progress.java
@@ -1,13 +1,12 @@
 
-
 public class Progress {
 
 	// この辺の値は要調整
 	private static final int playerCache = 100000;
 	private static final int dealerCache = 100000;
-	
-	public static void main(String args[]){
-		    Player player = new Player(new Wallet(playerCache));
-		    Player dealer = new Player(new Wallet(dealerCache));
+
+	public static void main(String args[]) {
+		Player player = new Player(new Wallet(playerCache));
+		Player dealer = new Player(new Wallet(dealerCache));
 	}
 }
