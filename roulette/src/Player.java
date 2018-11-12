e3c90575b66e908f0f8abed17a2f4c8d7c628daf
guni973 <ad2314ce71926@gmail.com>
2018-11-13 01:11:04 +0900

    掛け金の設定機能の追加

--- roulette/src/Player.java
+++ roulette/src/Player.java
@@ -1,7 +1,8 @@
 
 public class Player {
 	// Walletの実装次第
-	private Wallet wallet;
+	private Wallet wallet; // 所持金
+	private int coin; // 掛け金
 
 	public Player(Wallet _wallet) {
 		this.wallet = _wallet;
@@ -13,5 +14,17 @@ public class Player {
 
 	public void setWallet(Wallet _wallet) {
 		this.wallet = _wallet;
+	}
+
+	public void setCoin(int _coin) {
+		// TODO: 掛け金がWallet内にあるかどうかチェックする
+		// if ( this.wallet.getCache() - coin < 0) {
+		// return HasanException;
+		// }
+		this.coin = _coin;
+	}
+
+	public int getCoin(int _coin) {
+		return this.coin;
 	}
 }
