711dd30ca8e3170ceed082382b0a85015c180aa8
i13302 <s16t286@stu.kagawa-u.ac.jp>
2018-11-12 18:55:15 +0900

    要らないソースの削除

--- roulette/src/roulette/roulette.java
+++ roulette/src/roulette/roulette.java
@@ -3,14 +3,10 @@
  * Roulette メインクラス
  * 
  */
-import java.util.Random;
+
 public class roulette {
 	public static void main(String args[]) {
 		System.out.println("Hello! World!");
-		// Randomクラスのインスタンス化
-		Random random = new Random();
 
-		int randomValue =random.nextInt(10);
-		System.out.println(randomValue);
 	}
 }
