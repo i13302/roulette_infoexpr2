4f7265312fbf0307641975701fa285c2fe5e23eb
i13302 <s16t286@stu.kagawa-u.ac.jp>
2018-11-13 14:44:48 +0900

    ルーレット用にランダムな値を得る，クラスを生成

--- roulette/src/Progress.java
+++ roulette/src/Progress.java
@@ -1,5 +1,12 @@
 public class Progress {
-	  public static void main(String args[]){
-		    System.out.println("Hello! World!");
-		  }
+	public static void main(String args[]) {
+		System.out.println("Hello! World!");
+
+		Roulette roulette = new Roulette();
+
+		for (int i = 0; i < 38; i++) {
+			System.out.println(roulette.getStrRouletteValue());
+
+		}
+	}
 }
--- roulette/src/Roulette.java
+++ roulette/src/Roulette.java
@@ -1,11 +1,35 @@
 import java.util.Random;
 
 public class Roulette {
-	public static void main(String[] args) {
-		// Randomクラスのインスタンス化
-		Random random = new Random();
+	Random random;
 
-		int randomValue =random.nextInt(10);
-		System.out.println(randomValue);
+	private int minValue = 0; // 最低値
+	private int maxValue = 38; // 最大値
+
+	public Roulette() {
+		this.generateRandomInstance();
 	}
+
+	/* ランダムインスタンスを生成 */
+	public void generateRandomInstance() {
+		long seed = System.currentTimeMillis();
+		random=new Random(seed);
+	}
+	
+	/* ルーレットの値を数値として出す */
+	public int getIntRouletteValue() {
+		return  random.nextInt(this.maxValue) + this.minValue;
+	}
+	
+	/* ルーレットとしての値を出す． */
+	public String getStrRouletteValue() {
+		int v =this.getIntRouletteValue();
+
+		if (v == 37) { // 37の場合は00とする
+			return "00";
+		}
+		return Integer.toString(v);
+	}
+	
+
 }
