cd2b5bf5b663485da84c45fa53712ad7bbd0ff34
i13302 <s16t286@stu.kagawa-u.ac.jp>
2018-11-13 15:14:27 +0900

    format

--- roulette/src/Roulette.java
+++ roulette/src/Roulette.java
@@ -13,23 +13,22 @@ public class Roulette {
 	/* ランダムインスタンスを生成 */
 	public void generateRandomInstance() {
 		long seed = System.currentTimeMillis();
-		random=new Random(seed);
+		random = new Random(seed);
 	}
-	
+
 	/* ルーレットの値を数値として出す */
 	public int getIntRouletteValue() {
-		return  random.nextInt(this.maxValue) + this.minValue;
+		return random.nextInt(this.maxValue) + this.minValue;
 	}
-	
+
 	/* ルーレットとしての値を出す． */
 	public String getStrRouletteValue() {
-		int v =this.getIntRouletteValue();
+		int v = this.getIntRouletteValue();
 
 		if (v == 37) { // 37の場合は00とする
 			return "00";
 		}
 		return Integer.toString(v);
 	}
-	
 
 }
