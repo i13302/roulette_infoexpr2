ab572714504cddf7f2f0b42b6658d6bce1ce36a7
dayaman <s16t279@stu.kagawa-u.ac.jp>
2018-11-13 16:25:06 +0900

    str型の数字をget setするだけ

---
+++ roulette/src/Number.java
@@ -0,0 +1,14 @@
+
+public class Number {
+	private String _strnum;
+	public String get_strnum() {
+		return this._strnum;
+	}
+	public void set_strnum(String val) {
+		this._strnum = val;
+	}
+	String size;
+	String high_low;
+	String odd_eve;
+	String color;
+}
