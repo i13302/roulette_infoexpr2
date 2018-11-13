bad17c6cdaf53f604e348974fd117441ee203cc3
guni973 <ad2314ce71926@gmail.com>
2018-11-13 14:55:46 +0900

    refactor test code

--- roulette/src/WalletTest.java
+++ roulette/src/WalletTest.java
@@ -1,25 +1,29 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class WalletTest {
+	Wallet wallet;
+
+	@BeforeEach
+	void initialize() {
+		wallet = new Wallet(10000);
+	}
 
 	@Test
 	void testInitialize() {
-		Wallet wallet = new Wallet(10000);
 		assertEquals(10000, wallet.getCache());
 	}
 
 	@Test
 	void testPayCache() {
-		Wallet wallet = new Wallet(10000);
 		wallet.payCache(100);
 		assertEquals(9900, wallet.getCache());
 	}
 
 	@Test
 	void testIsInsolvency() {
-		Wallet wallet = new Wallet(10000);
 		wallet.payCache(10001);
 		assertTrue(wallet.isInsolvency());
 	}
