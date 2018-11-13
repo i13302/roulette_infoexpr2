684101c4f05a36320d47ed5009633cc76970bb1b
guni973 <ad2314ce71926@gmail.com>
2018-11-13 14:27:21 +0900

    支払い機能と持ち金の取得機能の追加

--- roulette/src/Wallet.java
+++ roulette/src/Wallet.java
@@ -6,6 +6,15 @@ public class Wallet {
 		this.cache = _cache;
 	}
 
+	public int getCache() {
+		return this.cache;
+	}
+
+	// setterは用意せず、支払いのみ設定
+	public void payCache(int _cache) {
+		this.cache -= _cache;
+	}
+
 	public boolean isInsolvency() {
 		return this.cache < 0;
 	}
