506e0c40bf44a767aeb9ad6a391ffbdb96423541
i13302 <s16t286@stu.kagawa-u.ac.jp>
2018-11-17 16:33:06 +0900

    インスタンスから，書き換えはできるけど，処理に時間がかかる．

--- roulette/src/Progress.java
+++ roulette/src/Progress.java
@@ -1,9 +1,18 @@
 
-
 public class Progress {
-	  public static void main(String args[]){
-				System.out.println("Hello! World!");
-				InforMation info =new InforMation();
-				System.out.println("Main");
-		  }
+	public static void main(String args[]) {
+		System.out.println("Hello! World!");
+		InforMation info = new InforMation();
+		for (int i = 0; i < 20; i++) {
+			System.out.println("Main");
+
+			try {
+				Thread.sleep(1 * 1000);
+			} catch (InterruptedException e) {
+
+			}
+			info.setMoney(i, i * 10);
+		}
+		System.exit(-1);
+	}
 }
