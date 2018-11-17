8d7a38c1786d95e53bd889d2650cd33cd4453dd4
dayaman <s16t279@stu.kagawa-u.ac.jp>
2018-11-17 17:29:09 +0900

    判定と、支払い処理の実装

--- roulette/src/Payment.java
+++ roulette/src/Payment.java
@@ -1,4 +1,19 @@
 
 public class Payment {
-	
+	public static void calc(Player player, Player dealer, int stopAddress) {
+		if (judge(player, stopAddress) == true) {
+			player.getWallet().payCache(player.getCoin()*36*(-1));
+			dealer.getWallet().payCache(player.getCoin()*36);
+		}else {
+			player.getWallet().payCache(player.getCoin());
+			dealer.getWallet().payCache(player.getCoin()*(-1));
+		}
+	}
+	private static boolean judge(Player player, int stopAddress) {
+		if (player.getAddress() == stopAddress) {
+			return true;
+		}else {
+			return false;
+		}
+	}
 }
