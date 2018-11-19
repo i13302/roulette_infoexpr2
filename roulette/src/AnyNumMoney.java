597abcd1b278ce2b52f1d5d94a6dbb0dbd4bf0fd
i13302 <s16t286@stu.kagawa-u.ac.jp>
2018-11-20 01:38:06 +0900

    一部の変数を変更

--- roulette/src/AnyNumMoney.java
+++ roulette/src/AnyNumMoney.java
@@ -7,7 +7,7 @@
 public class AnyNumMoney {
 	public static int init = -1;
 	public int num = this.init; // 賭けた場所
-	public static int num_len = 0; // 賭けた場所の番人
+	public static int numNext = 0; // 賭けた場所の番人
 	public int money = this.init; // 賭けたお金の額
-	public static int money_len = 0; // 賭けたお金の番人
+	public static int moneyNext = 0; // 賭けたお金の番人
 }
